e4dec64e207ab61123894d5c5b95b2b9ff72598d
Igor284 <kochat.lgor@gmail.com>
2021-11-25 20:11:59 +0200

    first commit

---
+++ SignalRClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRClient
+{
+    class Program
+    {
+        static HubConnection HubConnection;
+        static async Task Main(string[] args)
+        {
+            HubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:55536/notification")
+                .Build();
+
+            HubConnection.On<string>("Send", message => Console.WriteLine($"Massage from server: {message}"));
+            await HubConnection.StartAsync();
+
+            bool isExit = false;
+
+            while (!isExit)
+            {
+                var message = Console.ReadLine();
+                if (message != "exit")
+                    await HubConnection.SendAsync("SendMessage", message);
+                else
+                    isExit = true;
+            }
+            Console.ReadLine();
+        }
+    }
+}
